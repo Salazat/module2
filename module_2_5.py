d5d510736b48dfa22207da648c9f088b6ad8ca37
Salazat <166146734+Salazat@users.noreply.github.com>
2024-11-12 13:32:02 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        pustoy_spisok = []
+        for x in range(m):
+            pustoy_spisok.append(value)
+        matrix.append(pustoy_spisok)
+    return matrix
+m = int(input("Введите кол-во столбцов: "))
+n = int(input("Введите кол-во строк: "))
+value = int(input("Введите значение: "))
+a = get_matrix(m, n, value)
+print(a)
+
+
